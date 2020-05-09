a442449e90bc9f25beebf17be4616334d8d6bf1e
Vikas Shukla <shuk.vk@gmail.com>
2020-05-09 12:24:41 -0700

    implements Uploading feature (just needs link)

--- hacklarious-app/src/components/App.js
+++ hacklarious-app/src/components/App.js
@@ -1,22 +1,62 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Upload from './Upload';
-import styles from '../styles/App.module.css'
-import logo from '../styles/logo.png'
+import styles from '../styles/App.module.css';
+import logo from '../styles/logo.png';
+import axios from 'axios'
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [selectedFile, setSelectedFile] = useState(null);
+  //Post request Link goes here ---------------------------
+  const POST_IMAGE_URL = "";
+
   useEffect(() => {
     fetch("/api").then(res => res.json()).then(data => {
       setCurrentTime(data.hello)
 
     })
   })
+
+  const fileSelectorHandle = (event) => {
+    setSelectedFile(event.target.files[0]);
+  }
+
+  const fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append('image', selectedFile, selectedFile.name);
+    axios.post(POST_IMAGE_URL, fd)
+    .then(resp => console.log(resp))
+    .catch(err => {
+      console.log("Error with Uploading:", err);
+    })
+  }
+
+  const fileInput = useRef(null);
+
   return (
     <div>
     <img src={logo} alt="logo" style={{width:"400px", height:"auto", margin:"20px 100px"}}/>
     <Upload />
     <div className={styles.buttonDiv}>
-    <button type="button" className={`btn btn-lg btn-primary rounded-pill ${styles.button} shadow`}>Upload a Photo</button>
+    <input 
+      style={{display:"none"}}
+      type="file" 
+      onChange={fileSelectorHandle}
+      ref={fileInput}
+    />
+    <button type="button" 
+      className={`btn btn-lg btn-primary rounded-pill ${styles.button} shadow`}
+      onClick ={() => {fileInput.current.click()}}
+    >
+      Choose a Photo
+    </button>
+
+    <button type="button" 
+      className={`btn btn-lg btn-primary rounded-pill ${styles.button} shadow`}
+      onClick= {fileUploadHandler}
+    >
+      Upload
+    </button>
     <button type="button" className={`btn btn-lg btn-primary rounded-pill ${styles.button} shadow`}>Take a Photo</button>
     </div>
     </div>
