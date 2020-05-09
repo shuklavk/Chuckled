9e47d0d6d3b0e2d99623dd89286a587e272aa3b8
Muntaser Syed <jemsbhai@gmail.com>
2020-05-09 16:36:10 -0400

    server gets labels from image

--- hacklarious-app/backend/app.py
+++ hacklarious-app/backend/app.py
@@ -8,7 +8,7 @@ import requests
 from pymongo import MongoClient
 from pprint import pprint
 from google.cloud import datastore
-
+from google.cloud import vision
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -24,6 +24,82 @@ mongostr = creds["mongostr"]
 client = MongoClient(mongostr)
 
 db = client["jokes"]
+
+def get_top_labels(uri):
+    """Detects labels in the file located in Google Cloud Storage or on the
+    Web."""
+    # imageurl = "https://storage.googleapis.com/hacklarious/testimage.jpg"
+    
+    client = vision.ImageAnnotatorClient.from_service_account_json('gc.json')
+    image = vision.types.Image()
+    image.source.image_uri = uri
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    # print('Labels:')
+
+    i = 0
+
+    keywords = []
+    for label in labels:
+        print(label.description)
+        keywords.append(label.description)
+        i = i + 1
+        if i == 3:
+            break
+
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    if len(keywords) == 0:
+        keywords.append("random")
+    return keywords
+
+
+
+@app.route("/labelanimage", methods=['POST'])
+def labels():
+
+    print(request)
+
+    res = request.get_json()
+    print (res)
+
+    resraw = request.get_data()
+    print (resraw)
+
+##    args = request.args
+##    form = request.form
+##    values = request.values
+
+##    print (args)
+##    print (form)
+##    print (values)
+
+##    sres = request.form.to_dict()
+    imageurl = res["imgurl"]
+    labels = get_top_labels(imageurl)
+
+    status = {}
+    status["server"] = "up"
+    status["message"] = "some random message here"
+    status["request"] = res 
+    status["results"] = labels
+
+    statusjson = json.dumps(status)
+
+    print(statusjson)
+
+    js = "<html> <body>OK THIS WoRKS</body></html>"
+
+    resp = Response(statusjson, status=200, mimetype='application/json')
+    ##resp.headers['Link'] = 'http://google.com'
+
+    return resp
+
 
 
 
